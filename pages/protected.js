f1aea11772926ad14f027710f1242442f6b6d300
Allen Hai <allenhai03@gmail.com>
2019-07-23 09:49:14 -0500

    read base64 token from auth header

--- pages/protected.js
+++ pages/protected.js
@@ -2,11 +2,23 @@ function Protected() {
   return <h1>protected</h1>
 }
 
+
 Protected.getInitialProps = async ({ req, res}) => {
-  res.writeHead(401, {
-    'WWW-Authenticate': 'Basic realm=private page'
-  })
-  res.end('unauthorized')
+  // check for req, only available server side
+  if (req) {
+    const authHeader = req.headers['authorization']
+    const basicAuthToken = authHeader.split(' ')[1]
+    console.log('basic auth token', basicAuthToken)
+  }
+
+  // check for res, only available server side
+  if (res) {
+    res.writeHead(401, {
+      'WWW-Authenticate': 'Basic realm=private page'
+    })
+
+    res.end('unauthorized')
+  }
   return {}
 }
 
